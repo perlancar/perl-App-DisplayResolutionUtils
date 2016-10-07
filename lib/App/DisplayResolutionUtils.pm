c6e42937d1a4b0c4f4ae7b5fd8033e8b28a03613
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-10-07 09:00:10 +0700

    Initial commit

---
+++ lib/App/DisplayResolutionUtils.pm
@@ -0,0 +1,18 @@
+package App::DisplayResolutionUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLI utilities related to display resolution
+
+=head1 DESCRIPTION
+
+This distribution includes several utilities:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Display::Resolution>
