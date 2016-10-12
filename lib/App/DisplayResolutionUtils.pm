f189e2f9997d38bdd9d9879469949360373adc94
perlancar (@pc-office) <perlancar@gmail.com>
2016-10-12 14:03:57 +0700

    list-display-resolution-names(): Allow filtering by query argument

--- lib/App/DisplayResolutionUtils.pm
+++ lib/App/DisplayResolutionUtils.pm
@@ -3,6 +3,47 @@ package App::DisplayResolutionUtils;
 # DATE
 # VERSION
 
+use 5.010001;
+use strict;
+use warnings;
+
+our %SPEC;
+
+$SPEC{list_display_resolution_names} = {
+    v => 1.1,
+    summary => 'List known display resolution names',
+    args => {
+        query => {
+            schema => 'str*',
+            pos => 0,
+        },
+    },
+    result => {
+        schema => ['hash*', of=>'str*'],
+    },
+    result_naked => 1,
+};
+sub list_display_resolution_names {
+    require Display::Resolution;
+
+    my %args = @_;
+
+    my $q = lc($args{query} // '');
+
+    my $res0 = Display::Resolution::list_display_resolution_names();
+    my $res = {};
+
+    for (keys %$res0) {
+        if (length $q) {
+            next unless index(lc($_), $q) >= 0 ||
+                index(lc($res0->{$_}), $q) >= 0;
+        }
+        $res->{$_} = $res0->{$_};
+    }
+
+    $res;
+}
+
 1;
 # ABSTRACT: CLI utilities related to display resolution
 
